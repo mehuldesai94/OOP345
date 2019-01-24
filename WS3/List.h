59445feccdb61ddbff411af8a83ab057228e8c6a
Mehul Desai <desaimehul94@gmail.com>
2019-01-24 18:33:26 -0500

    Add files via upload

---
+++ WS3/List.h
@@ -0,0 +1,26 @@
+#ifndef W3_LIST_H
+#define W3_LIST_H
+
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+template<class T, int N>
+class List {
+	T a[N]; //T and N are specified by client code. a is an array of any data type
+	size_t count;
+public:
+	List() { };
+	size_t size() const { return count; };   //returns the number of entries in array a
+	const T& operator[](int index) const { return a[index]; };  //returns requested array index
+	void operator+=(const T& n) {
+		if (size() < N) {
+			a[size()] = n; //Adds n to a IF SPACE IS AVAILABLE
+			count++;
+		}
+	};
+};
+
+
+#endif // !W3_LIST_H
---
+++ WS3/Pair.h
@@ -0,0 +1,20 @@
+#ifndef W3_PAIR_H
+#define W3_PAIR_H
+
+
+#include <iostream>
+using namespace std;
+
+template<class A, class B>
+class Pair {
+	A a;
+	B b;
+public:
+	Pair() { a = A(), b = B(); };  //adopts safe empty state
+	Pair(const A& ra, const B& rb) { a = ra, b = rb; };
+	const A& getKey() const { return a; };  //NOT SURE
+	const B& getValue() const { return b; };
+};
+
+#endif // !W3_PAIR_H
+#pragma once
